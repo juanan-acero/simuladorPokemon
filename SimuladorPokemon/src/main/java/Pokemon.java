e252ef68f8ee4fe9f624813ccf9f76640f488667
jonmimtz <jonmimtz@gmail.com>
2020-06-05 23:30:42 +0200

    Se ha puesto atk  squirtle

--- SimuladorPokemon/src/main/java/Pokemon.java
+++ SimuladorPokemon/src/main/java/Pokemon.java
@@ -1,17 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pokemon {
     String nombre;
     Tipo tipo;
     Integer ps;
     Integer velocidad;
-    Movimientos[] movimientos;
+    List<Movimientos>  movimientos;
 
-    public Pokemon(String nombre, Tipo tipo, Integer ps, Integer velocidad, Movimientos[] movimientos) {
+    public Pokemon(String nombre, Tipo tipo, Integer ps, Integer velocidad, List<Movimientos> movimientos) {
         this.nombre = nombre;
         this.tipo = tipo;
         this.ps = ps;
         this.velocidad = velocidad;
-        this.movimientos = new Movimientos[4];
+        this.movimientos = new ArrayList<Movimientos>();
         this.movimientos = movimientos;
+
 
     }
 }
+
--- SimuladorPokemon/src/main/java/SimuladorPokemon.java
+++ SimuladorPokemon/src/main/java/SimuladorPokemon.java
@@ -19,16 +19,42 @@ public class SimuladorPokemon {
         efe.add("Acero");
         tipoFuego.setDebilidades(efe);
 
-        Movimientos[] movimientosCharmander = new Movimientos[4];
+
+        List<Movimientos> listamovchar = new ArrayList<Movimientos>();
+        listamovchar.add(new Movimientos("Fuego", 80, 75));
+        listamovchar.add(new Movimientos("Fuego", 80, 75));
+        listamovchar.add(new Movimientos("Fuego", 80, 75));
+        listamovchar.add(new Movimientos("Fuego", 80, 75));
+        List<Movimientos> listamovpik = new ArrayList<Movimientos>();
+        listamovpik.add(new Movimientos("Electrico", 80, 75));
+        listamovpik.add(new Movimientos("Electrico", 80, 75));
+        listamovpik.add(new Movimientos("Electrico", 80, 75));
+        listamovpik.add(new Movimientos("Electrico", 80, 75));
+        List<Movimientos> listamovsquir = new ArrayList<Movimientos>();
+        listamovsquir.add(new Movimientos("Agua", 80, 75));
+        listamovsquir.add(new Movimientos("Agua", 80, 75));
+        listamovsquir.add(new Movimientos("Agua", 80, 75));
+        listamovsquir.add(new Movimientos("Agua", 80, 75));
+
+/*        Movimientos[] movimientosCharmander = new Movimientos[4];
         Movimientos[] movimientosPikachu = new Movimientos[4];
-        movimientosCharmander[0] = new Movimientos("Normal", 120, 75);
-        movimientosPikachu[0] = new Movimientos("Normal", 120, 75);
-        Pokemon pikachu = new Pokemon("Pikachu", tipoFuego, 50, 50, movimientosPikachu);
+        Movimientos[] movimientosSquirtle = new Movimientos[4];
+
+        movimientosCharmander[0] = (Movimientos) listamovchar;
+        movimientosPikachu [0] = (Movimientos) listamovpik;
+*/
+
+        System.out.println("La potencia impresa es más de "+listamovchar.get(0).potencia );
+        System.out.println("La potencia impresa es más de "+listamovpik.get(0).potencia );
+        System.out.println("El pokemon impreso es más "+listamovsquir.get(0).potencia );
+
+
+        Pokemon pikachu = new Pokemon("Pikachu", tipoFuego, 50, 50, listamovpik);
 
         List<Pokemon> equipoPaco = new ArrayList<Pokemon>();
-        equipoPaco.add(new Pokemon("Charmander", tipoFuego, 50, 50, movimientosCharmander));
+        equipoPaco.add(new Pokemon("Charmander", tipoFuego, 50, 50, listamovchar));
         List<Pokemon> equipoPepe = new ArrayList<Pokemon>();
-        equipoPepe.add(new Pokemon("Charmander", tipoFuego, 50, 50, movimientosCharmander));
+        equipoPepe.add(new Pokemon("Charmander", tipoFuego, 50, 50, listamovchar));
 
 
         Combate combate = new Combate(equipoPaco.get(0), equipoPepe.get(0));
@@ -36,9 +62,9 @@ public class SimuladorPokemon {
 
         List<Pokemon> ejemploLista = new ArrayList<Pokemon>();
 
-        ejemploLista.add(new Pokemon("Charmander", tipoFuego, 50, 50, movimientosCharmander));
-        ejemploLista.add(new Pokemon("Pikachu", tipoFuego, 50, 50, movimientosCharmander));
-        ejemploLista.add(new Pokemon("Squirtle", tipoFuego, 50, 50, movimientosCharmander));
+        ejemploLista.add(new Pokemon("Charmander", tipoFuego, 50, 50, listamovchar));
+        ejemploLista.add(new Pokemon("Pikachu", tipoFuego, 50, 50, listamovpik));
+        ejemploLista.add(new Pokemon("Squirtle", tipoFuego, 50, 50, listamovsquir));
 
         System.out.println("Tenemos un total de " + ejemploLista.size() + " pokemon en el equipo");
 
@@ -50,7 +76,7 @@ public class SimuladorPokemon {
 
     }
 
-    public static void meterPokemon(List<Pokemon> lista, String nombre, Tipo tipo, Integer ps, Integer velocidad, Movimientos[] movimientos) {
+    public static void meterPokemon(List<Pokemon> lista, String nombre, Tipo tipo, Integer ps, Integer velocidad, List<Movimientos> movimientos) {
         lista.add(new Pokemon(nombre, tipo, ps, velocidad, movimientos));
     }
 
